188f7ad7b3756295243b6b098e198f99a4268b6f
rahmatagungj <rahmatagungj@gmail.com>
2021-05-28 10:01:25 +0700

    add script for function

---
+++ Method 1/func.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("img").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const image = event.target.getAttribute("data-src");
+    event.target.setAttribute("src", image);
+  });
+});
